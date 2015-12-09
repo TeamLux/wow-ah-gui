e049d3b91dbdf6cad1d519bb70993a78b86ccf74
Antoine Cayphas <antoine_cayphas@hotmail.com>
2015-12-09 14:18:40 +0100

    add week and day graph

--- src/drawCharts.js
+++ src/drawCharts.js
@@ -41,7 +41,7 @@ function drawCharts(){
 	// google.setOnLoadCallback(drawChart);
 }
 
-function addHead(a,datas,isStd,isMax,isMin,isBid){
+function addHead(a,datas,isStd,isMax,isMin,isBid,isAvg){
 	var name;
 	if(a==0)
 		name = "buy";
@@ -55,9 +55,12 @@ function addHead(a,datas,isStd,isMax,isMin,isBid){
 	if(datas[0].length==0)
 		datas[0].push("Date");
 	datas[0].push("Q_"+name);
-	datas[0].push(name);
-	if(isStd)
-		datas[0].push("std_"+name);
+
+	if(isAvg){
+		datas[0].push("avg_"+name);
+		if(isStd)
+			datas[0].push("avg_std_"+name);
+	}
 	if(isMax){
 		datas[0].push("max_"+name);
 		if(isStd)
@@ -72,20 +75,10 @@ function addHead(a,datas,isStd,isMax,isMin,isBid){
 		datas[0].push("bid_"+name);
 		if(isStd)
 			datas[0].push("bid_std_"+name);
-		if(isMax){
-			datas[0].push("bid_max_"+name);
-			if(isStd)
-				datas[0].push("bid_max_std_"+name);
-		}
-		if(isMin){
-			datas[0].push("bid_min_"+name);
-			if(isStd)
-				datas[0].push("bid_min_std_"+name);
-		}
 	}
 }
 
-function addData(i,datas,isStd,isMax,isMin,isBid,line){
+function addData(i,datas,isStd,isMax,isMin,isBid,isAvg,line){
 	data = line.split("\t")
 
 	if(datas[i].length==0){
@@ -95,9 +88,13 @@ function addData(i,datas,isStd,isMax,isMin,isBid,line){
 	//Date,inter,Q,max,min,avg,std,max,min,avg,std
 	// 0  , 1   ,2, 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10
 	datas[i].push(parseInt(data[2]));
-	datas[i].push(parseInt(data[5]));
-	if(isStd)
-		datas[i].push(parseInt(data[6]));
+
+	if(isAvg){
+		datas[i].push(parseInt(data[5]));
+		if(isStd)
+			datas[i].push(parseInt(data[6]));
+	}
+
 	if(isMax){
 		datas[i].push(parseInt(data[3]));
 		if(isStd)
@@ -112,17 +109,83 @@ function addData(i,datas,isStd,isMax,isMin,isBid,line){
 		datas[i].push(parseInt(data[9]));
 		if(isStd)
 			datas[i].push(parseInt(data[10]));
-		if(isMax){
-			datas[i].push(parseInt(data[7]));
-			if(isStd)
-				datas[i].push(0);
-		}
-		if(isMin){
-			datas[i].push(parseInt(data[8]));
-			if(isStd)
-				datas[i].push(0);
-		}
 	}
+}
+
+function weekData(datas){
+	var i;
+	var j;
+	wDatas = new Array();
+	wDatas.push(new Array());
+	wN = new Array();
+	// Init the head
+	for(j = 0; j < datas[0].length; j++){
+		wDatas[0].push(datas[0][j]);
+		wN.push(0)
+	}
+	//Init the Date
+	for(i = 0; i < 24*7; i++){
+		wDatas.push(new Array());
+		wDatas[i+1].push(new Date(2015,10,parseInt(i/24)+1,i%24,30,0));
+		for(j = 1; j < datas[0].length; j++){
+			wDatas[i+1].push(0);
+		}
+		wN.push(0);
+	}
+	//Fill the wData
+	for(i = 1; i < datas.length; i++){
+		date = datas[i][0];
+		id = date.getUTCDay()*24+date.getHours()+1;
+		for(j = 1; j< datas[j].length; j++){
+			wDatas[id][j] += datas[i][j]
+		}
+		wN[id]+=1
+	}
+	for(i = 1; i <= 24*7; i++){
+		for(j = 1; j < datas[0].length; j++){
+			wDatas[i][j]/=wN[i];
+		}
+
+	}
+	return wDatas
+}
+
+function dayData(datas){
+	var i;
+	var j;
+	wDatas = new Array();
+	wDatas.push(new Array());
+	wN = new Array();
+	// Init the head
+	for(j = 0; j < datas[0].length; j++){
+		wDatas[0].push(datas[0][j]);
+		wN.push(0)
+	}
+	//Init the Date
+	for(i = 0; i < 24; i++){
+		wDatas.push(new Array());
+		wDatas[i+1].push(new Date(2015,10,1,i,30,0));
+		for(j = 1; j < datas[0].length; j++){
+			wDatas[i+1].push(0);
+		}
+		wN.push(0);
+	}
+	//Fill the wData
+	for(i = 1; i < datas.length; i++){
+		date = datas[i][0];
+		id = date.getHours()+1;
+		for(j = 1; j< datas[j].length; j++){
+			wDatas[id][j] += datas[i][j]
+		}
+		wN[id]+=1
+	}
+	for(i = 1; i <= 24; i++){
+		for(j = 1; j < datas[0].length; j++){
+			wDatas[i][j]/=wN[i];
+		}
+
+	}
+	return wDatas
 }
 
 function google_table_price(datas,isStd,nType){
@@ -238,9 +301,10 @@ function drawChart(){
 	var nType = isBuy+isNotbuy+isSale+isUptosale
 
 	var isStd = form.adds[0].checked;
-	var isMax = form.adds[1].checked;
-	var isMin = form.adds[2].checked;
-	var isBid = form.adds[3].checked;
+	var isAvg = form.adds[1].checked;
+	var isMax = form.adds[2].checked;
+	var isMin = form.adds[3].checked;
+	var isBid = form.adds[4].checked;
 
 	var allTexts = new Array();
 
@@ -266,13 +330,16 @@ function drawChart(){
 	for(var a = 0;a<4;a++){
 		if(!form.datas[a].checked)
 			continue;
-		addHead(a,datas,isStd,isMax,isMin,isBid);
+		addHead(a,datas,isStd,isMax,isMin,isBid,isAvg);
 		for(i =0; i < allTexts[a].length-1;i++){
 			if(datas[i+1]==undefined)
 				datas[i+1] = new Array();
-			addData(i+1,datas,isStd,isMax,isMin,isBid,allTexts[a][i]);
+			addData(i+1,datas,isStd,isMax,isMin,isBid,isAvg,allTexts[a][i]);
 		}
 	}
+	var week_datas = weekData(datas)
+	var day_datas = dayData(datas)
+
 	var google_datas_price = google_table_price(datas,isStd,nType);
 
 	var dygraph_datas_price = dygraph_csv_price(datas, isStd,nType);
@@ -280,6 +347,12 @@ function drawChart(){
 	var google_datas_Q = google_table_Q(datas,nType);
 
 	var dygraph_datas_Q = dygraph_csv_Q(datas,nType);
+
+	var week_price = dygraph_csv_price(week_datas,isStd,nType);
+	var week_Q = dygraph_csv_Q(week_datas,nType);
+
+	var day_price = dygraph_csv_price(day_datas,isStd,nType);
+	var day_Q = dygraph_csv_Q(day_datas,nType);
 
 	var chart_price = new google.visualization.AnnotatedTimeLine(document.getElementById('gviz_div'));
  	chart_price.draw(google_datas_price,  {displayAnnotations: true});
@@ -296,6 +369,36 @@ function drawChart(){
  	chart_price.draw(google_datas_Q,  {displayAnnotations: true});
 
  	var g_price = new Dygraph(document.getElementById("dg_div_q"),dygraph_datas_Q,
+          {
+            rollPeriod: 1,
+            showRoller: true,
+            legend: 'always'
+          });
+
+ 	var g_week_price = new Dygraph(document.getElementById("week"),week_price,
+          {
+            rollPeriod: 1,
+            showRoller: true,
+            errorBars : isStd,
+            legend: 'always'
+          });
+
+ 	var g_week_Q = new Dygraph(document.getElementById("week_q"),week_Q,
+          {
+            rollPeriod: 1,
+            showRoller: true,
+            legend: 'always'
+          });
+
+ 	var g_day_price = new Dygraph(document.getElementById("day"),day_price,
+          {
+            rollPeriod: 1,
+            showRoller: true,
+            errorBars : isStd,
+            legend: 'always'
+          });
+
+ 	var g_day_Q = new Dygraph(document.getElementById("day_q"),day_Q,
           {
             rollPeriod: 1,
             showRoller: true,
